e5f820417277e754e4407463e18aba0e3ea31476
APTECH <aryaputra.pro25@gmail.com>
2026-05-11 11:39:19 +0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -11,11 +11,10 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-    const notificationTitle = payload.notification.title || "Ada Pesan Baru!";
+    const notificationTitle = payload.notification.title || "Duit Masuk!";
     const notificationOptions = {
-        body: payload.notification.body || "Cek aplikasi sekarang",
+        body: payload.notification.body || "Cek saldo sekarang!",
         icon: payload.notification.image || 'https://cdn-icons-png.flaticon.com/512/3135/3135706.png',
     };
-
     self.registration.showNotification(notificationTitle, notificationOptions);
 });
