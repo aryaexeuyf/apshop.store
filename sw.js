815812b1466011cbc7d532536adf5f3eb6468674
APTECH <aryaputra.pro25@gmail.com>
2026-05-11 11:11:51 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,23 @@
-self.addEventListener('push', function(event) {
-    const options = {
-        body: event.data.text(),
-        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135706.png',
-        vibrate: [100, 50, 100],
-        data: { dateOfArrival: Date.now() }
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCgWhNLaeV8vE1bNLtU32JYafvxbtFXU-o",
+    projectId: "ap-shop-b4e7e",
+    messagingSenderId: "370581375566",
+    appId: "1:370581375566:web:2dace12adc2e19686a511f"
+});
+
+const messaging = firebase.messaging();
+
+// Menangani notifikasi saat aplikasi di background
+messaging.onBackgroundMessage((payload) => {
+    console.log('Background Message:', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: payload.notification.image || 'https://cdn-icons-png.flaticon.com/512/3135/3135706.png',
+        vibrate: [200, 100, 200]
     };
-    event.waitUntil(
-        self.registration.showNotification('Duit Notification', options)
-    );
+    self.registration.showNotification(notificationTitle, notificationOptions);
 });
